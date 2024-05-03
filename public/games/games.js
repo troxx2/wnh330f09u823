902565f870d4092d896d4b7b0a572213ba742793
troxx2 <152228281+troxx2@users.noreply.github.com>
2024-05-03 14:10:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ public/games/games.js
@@ -0,0 +1,27 @@
+fetch('games.json')
+    .then(response => response.json())
+    .then(games => {
+        games.sort((a, b) => a.name.localeCompare(b.name));
+
+        const container = document.querySelector('.g-container');
+        container.innerHTML = '';
+        const gamecdn = "https://glcdn.githack.com/kaioxdev/legacy-assets/-/raw/main/"
+
+        games.forEach(game => {
+            const link = document.createElement('a');
+            link.className = 'g';
+            link.href = gamecdn + game.root + "/" + game.file;
+
+            const img = document.createElement('img');
+            img.src = gamecdn + game.root + "/" + game.img;
+
+            const h3 = document.createElement('h3');
+            h3.textContent = game.name;
+
+            link.appendChild(img);
+            link.appendChild(h3);
+
+            container.appendChild(link);
+        });
+    })
+    .catch(error => console.error('Error:', error));
---
+++ public/uv/js/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const form = document.getElementById("uv-form");
+const address = document.getElementById("uv-address");
+const searchEngine = document.getElementById("uv-search-engine");
+const error = document.getElementById("uv-error");
+const errorCode = document.getElementById("uv-error-code");
+
+const url = new URLSearchParams(window.location.search);
+const q = url.get('q');
+if (q) {
+  const url = search(q, searchEngine.value);
+  localStorage.setItem('url', __uv$config.prefix + __uv$config.encodeUrl(url));
+  location.href = "/load.html"
+} else {
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, localStorage.getItem('searchEngine'));
+  localStorage.setItem('url', __uv$config.prefix + __uv$config.encodeUrl(url));
+  location.href = "/load.html"
+});
